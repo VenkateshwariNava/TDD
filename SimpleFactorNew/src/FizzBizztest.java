2291404b181a1825258b34ef1eb02a78a165525e
Venkateshwari  Navaneetham <venkateshwari@nsearchglobal.com>
2019-05-31 15:26:07 +0800

    FizzBizz function using Junit for TDD

---
+++ SimpleFactorNew/src/FizzBizztest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+
+
+public  class FizzBizztest {
+
+
+   private final FizzBizz fizzbizz = new FizzBizz();
+ 
+   @Test
+   public void returnsNumber() throws Exception {
+	   assertEquals("Fizz", fizzbizz.say(3));
+	   assertEquals("Buzz", fizzbizz.say(5));
+	   assertEquals("Fizz Buzz", fizzbizz.say(15));
+   }
+ 
+   
+}
