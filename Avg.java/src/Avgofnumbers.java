254ac8b760f1b41ed465a75c083667b1b695892c
Venkateshwari  Navaneetham <venkateshwari@nsearchglobal.com>
2019-06-03 12:42:23 +0800

    Junit test for Avg of numbers

---
+++ Avg.java/src/Avgofnumbers.java
@@ -0,0 +1,14 @@
+
+public class Avgofnumbers {
+	
+	 public static String avg(String number) {
+		 
+		 
+	
+	       if (number == "") {
+	         
+	         
+	       }
+	       return "N/A";
+	 }
+}
---
+++ Avg.java/src/Avgtest.java
@@ -0,0 +1,16 @@
+	import static org.junit.Assert.assertEquals;
+
+	import org.junit.Test;
+
+
+public class Avgtest {
+		
+	@Test
+	    public  void A() {
+	    	assertEquals("N/A", Avgofnumbers.avg(""));
+	    }
+	
+//public void b() {
+	    	//assertEquals(0, Avgofnumbers.avg("Two,Three,four"));
+	   // }
+}
